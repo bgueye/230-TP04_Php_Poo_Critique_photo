f0ca05bd0dbbb016e196adc369f2bab5f945903d
Boubacar <bgueye@gmail.com>
2021-05-06 11:14:28 +0200

    modifs viewPhoto et methode newPhoto

--- Src/Controllers/PhotoController.php
+++ Src/Controllers/PhotoController.php
@@ -14,33 +14,35 @@ use App\Src\Models\UserModel;
  *
  * @author Boubacar
  */
-class PhotoController extends Controller {
-    
-    public function index() {
-        
+class PhotoController extends Controller
+{
+
+    public function index()
+    {
+
         $model = new PhotoModel();
         $photos = $model->listPhotos();
         //var_dump($photos);
         $this->render('photo/viewPhoto', ['photos' => $photos]);
     }
-    
-    public function voir($id) {
-        
-        if (!empty($_POST['contenu'])){
-        $contenu = filter_input(INPUT_POST, 'contenu', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    public function voir($id)
+    {
+
+        if (!empty($_POST['contenu'])) {
+            $contenu = filter_input(INPUT_POST, 'contenu', FILTER_SANITIZE_SPECIAL_CHARS);
 
             $user = new UserModel;
             $currentUser = $user->findByLogin($_SESSION['login']);
             $idUser = $currentUser->id;
-            
+
             $newComment = new CommentModel;
-            
+
             $newComment->setContenu($contenu);
             $newComment->setidPhoto($id);
             $newComment->setUser($idUser);
             $newComment->setVisible(1);
             $newComment->create();
-
         }
 
         $modelPhoto = new PhotoModel();
@@ -50,52 +52,47 @@ class PhotoController extends Controller {
         $modelComment = new CommentModel;
         $Comments = $modelComment->findByPhotoId($photo->id);
         //$tab = get_object_vars($photo);
-        
+
         $this->render('photo/voirPhoto', [
             'photo' => $photo,
             'comments' => $Comments
         ]);
     }
-    
 
 
-    public function newPhoto(){
+
+    public function newPhoto()
+    {
 
         //var_dump($_POST);
-        //print_r($_FILES);
-        if (FormBuilder::validate($_POST, ['titre']) && isset($_FILES['photo']) && $_FILES['photo']['name'] <= 5000000)
-        { 
-            // Testons si le fichier n'est pas trop gros
-            if ($_FILES['photo']['size'] <= 5000000)
-            {
-                // Testons si l'extension est autorisée
-                $infosfichier = pathinfo($_FILES['photo']['name']);
-                $extension_upload = $infosfichier['extension'];
-                $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
-                if (in_array($extension_upload, $extensions_autorisees))
-                {
-                    $titre = filter_input(INPUT_POST, 'titre', FILTER_SANITIZE_SPECIAL_CHARS);
+        print_r($_FILES);
+        if (FormBuilder::validate($_POST, ['titre']) && isset($_FILES['photo']) && $_FILES['photo']['size'] <= 5000000) {
 
-                    try{
-                        $namePhoto = 'photos/'. basename($_FILES['photo']['name']);
-                        $user = new UserModel;
-                        $currentUser = $user->findByLogin($_SESSION['login']);
-                        $idUser = $currentUser->id;
+            // Testons si l'extension est autorisée
+            $infosfichier = pathinfo($_FILES['photo']['name']);
+            $extension_upload = $infosfichier['extension'];
+            $extensions_autorisees = array('jpg', 'jpeg', 'gif', 'png');
+            if (in_array($extension_upload, $extensions_autorisees)) {
+                $titre = filter_input(INPUT_POST, 'titre', FILTER_SANITIZE_SPECIAL_CHARS);
 
-                        $newPhoto = new PhotoModel;
-                        $newPhoto->setTitlePhoto($titre);
-                        $newPhoto->setNameFile($namePhoto);
-                        $newPhoto->setidUser($idUser);
-                        
-                        $newPhoto->create();
-                        // On peut valider le fichier et le stocker définitivement
-                        move_uploaded_file($_FILES['photo']['tmp_name'], $namePhoto);
-                    }catch(Exception $e){
-                        $e->getMessage();
-                    }
+                try {
+                    $namePhoto = 'photos/' . basename($_FILES['photo']['name']);
+                    $user = new UserModel;
+                    $currentUser = $user->findByLogin($_SESSION['login']);
+                    $idUser = $currentUser->id;
+
+                    $newPhoto = new PhotoModel;
+                    $newPhoto->setTitlePhoto($titre);
+                    $newPhoto->setNameFile($namePhoto);
+                    $newPhoto->setidUser($idUser);
+
+                    $newPhoto->create();
+                    // On peut valider le fichier et le stocker définitivement
+                    move_uploaded_file($_FILES['photo']['tmp_name'], $namePhoto);
+                } catch (Exception $e) {
+                    $e->getMessage();
                 }
             }
-              
         }
 
 
@@ -108,7 +105,7 @@ class PhotoController extends Controller {
             ->ajoutLabelFor('titre', 'Titre')
             ->ajoutInput('text', 'titre', ['id' => 'tire', 'class' => 'form-control'])
             ->ajoutLabelFor('photo', '')
-            ->ajoutInput('file', 'photo', ['id' => 'photo','accept' =>'.png, .jpg, .jpeg', 'class' => 'form-control'])
+            ->ajoutInput('file', 'photo', ['id' => 'photo', 'accept' => '.png, .jpg, .jpeg', 'class' => 'form-control'])
             ->ajoutBouton('Ajouter', ['class' => 'btn btn-primary btn-sm'])
             ->finForm();
 
@@ -125,18 +122,17 @@ class PhotoController extends Controller {
             ->ajoutLabelFor('pseudo', 'Pseudo')
             ->ajoutInput('text', 'titre', ['id' => 'tire', 'class' => 'form-control'])
             ->ajoutLabelFor('photo', '')
-            ->ajoutInput('file', 'photo', ['id' => 'photo','accept' =>'.png, .jpg, .jpeg', 'class' => 'form-control'])
+            ->ajoutInput('file', 'photo', ['id' => 'photo', 'accept' => '.png, .jpg, .jpeg', 'class' => 'form-control'])
             ->ajoutBouton('Ajouter', ['class' => 'btn btn-primary btn-sm'])
             ->finForm();
 
         // On envoie le formulaire à la vue en utilisant notre méthode "create"
         $this->render('photo/formPhoto', ['newPhotoForm' => $formPhoto->create()]);
-
     }
 
     public function delatePhoto()
     {
-        if (!empty($_GET['id'])){
+        if (!empty($_GET['id'])) {
             $idPhoto = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
             //on vérifie si la photo existe dans la base de données
@@ -144,26 +140,23 @@ class PhotoController extends Controller {
             $model = new PhotoModel;
             $photo = $model->find($idPhoto);
             //var_dump($photo);
-            if (!empty($photo) && $photo->id_user == $_SESSION['id']){
+            if (!empty($photo) && $photo->id_user == $_SESSION['id']) {
 
                 //On doit supprimer d'abord tous les commentaires de cette photo
-                try{
-                     $model->delete($idPhoto);
-                }catch(Exception $e){
+                try {
+                    $model->delete($idPhoto);
+                } catch (Exception $e) {
                     $e->getMessage();
                 }
-               
             }
             header('Location: index.php?entite=user');
-            
         }
-        
     }
 
 
     public function delateComment()
     {
-        if (!empty($_GET['id'])){
+        if (!empty($_GET['id'])) {
             $idComment = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
             //on vérifie si le commentaire existe dans la base de données
@@ -172,18 +165,15 @@ class PhotoController extends Controller {
             $comment = $model->find($idComment);
             //var_dump($comment);
             //die();
-            if (!empty($comment) && $comment->id_user == $_SESSION['id']){
+            if (!empty($comment) && $comment->id_user == $_SESSION['id']) {
 
-                try{
+                try {
                     $model->delete($idComment);
-                }catch(Exception $e){
+                } catch (Exception $e) {
                     $e->getMessage();
                 }
-               
             }
             header('Location: index.php?entite=user');
-            
         }
-        
     }
 }
--- Src/Views/photo/voirPhoto.php
+++ Src/Views/photo/voirPhoto.php
@@ -8,27 +8,27 @@
           <p class="card-text">
             <small class="text-muted">Last updated <?= $photo->post_at ?></small>
             <?php if (!empty($_SESSION['login'])) { ?>
-            <a href="index.php?entite=photo&action=delatePhoto&id=<?= $photo->id ?>" class="badge btn-danger">Supprimer</a>
+              <a href="index.php?entite=photo&action=delatePhoto&id=<?= $photo->id ?>" class="badge btn-danger">Supprimer</a>
             <?php } ?>
           </p>
-        
-        <?php foreach($comments as $comment){ ?>
-          <div class="card">
-          <div class="card-body">
-            <h5 class="card-title"><?php if(!empty($_SESSION['pseudo'])){echo $_SESSION['pseudo'];} ?></h5>
-            <p class="card-text"><small class="text-muted">Publié le : <?= $comment->create_at ?></small></p>
-            <p class="card-text">Commentaire : <?= $comment->comment ?></p>
-            <?php if (!empty($_SESSION['login'])) { ?>
-            <small class="badge btn-danger"><a href="index.php?entite=photo&action=delateComment&id=<?= $comment->id ?>">Supprimer</a></small>
-            <?php } ?>
-          </div>
-        </div>
-        <?php
-        }
-        ?>
-        
-        
-        
+
+          <?php foreach ($comments as $comment) { ?>
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title"><?php if (!empty($_SESSION['pseudo'])) { echo $_SESSION['pseudo']; } ?></h5>
+                <p class="card-text"><small class="text-muted">Publié le : <?= $comment->create_at ?></small></p>
+                <p class="card-text">Commentaire : <?= $comment->comment ?></p>
+                <?php if (!empty($_SESSION['login'])) { ?>
+                  <small class="badge btn-danger"><a href="index.php?entite=photo&action=delateComment&id=<?= $comment->id ?>">Supprimer</a></small>
+                <?php } ?>
+              </div>
+            </div>
+          <?php
+          }
+          ?>
+
+
+
         </div>
       </div>
     </div>
@@ -38,16 +38,16 @@
     <div class="card">
     <form action="#" method="post">
       <label for="pseudo">Pseudo</label>
-      <input type="text" name="pseudo" id="pseudo" value="'.$_SESSION['pseudo'].'" class="form-control">
+      <input type="text" name="pseudo" id="pseudo" value="' . $_SESSION['pseudo'] . '" class="form-control">
       <label for="email">Email</label>
-      <input type="email" name="email" id="email" value="'.$_SESSION['login'].'" class="form-control">
+      <input type="email" name="email" id="email" value="' . $_SESSION['login'] . '" class="form-control">
       <label for="contenu">Message :</label>
       <textarea name="contenu" id="contenu" class="form-control"></textarea>
       <button class="badge btn-primary btn-sm">Envoyer</button>
     </form>
     </div>
     </div>';
-      } ?>
-    </div>
+    } ?>
   </div>
+</div>
 </div>
